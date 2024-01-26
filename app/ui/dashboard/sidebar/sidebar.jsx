54326994aa44f71bcd1055b8a1545fed96721051
Rajesh Tiwari <tiwarirj73@gmail.com>
2024-01-26 15:48:29 +0530

    side bar completed

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,79 +1,95 @@
+import Image from "next/image";
+import MenuLink from "./menuLinks/menuLink";
 import styles from "./sidebar.module.css";
-import {MdAttachMoney, MdDashboard, MdOutlineSettings, MdShoppingBag, MdSupervisedUserCircle} from 'react-icons/md'
+import {
+  MdAttachMoney,
+  MdDashboard,
+  MdLogout,
+  MdOutlineSettings,
+  MdShoppingBag,
+  MdSupervisedUserCircle,
+} from "react-icons/md";
 const menuItems = [
   {
     title: "Pages",
-    list:[
-     { title: "Dashboard",
-      path: "/dashboard",
-      icon: <MdDashboard/>,
-    },
-     { title: "Users",
-      path: "/dashboard",
-      icon: <MdSupervisedUserCircle/>,
-    },
-     { title: "Products",
-      path: "/dashboard/products",
-      icon: <MdShoppingBag/>,
-    },
-     { title: "Transactions",
-      path: "/dashboard/transactions",
-      icon: <MdAttachMoney/>,
-    },
+    list: [
+      { title: "Dashboard", path: "/dashboard", icon: <MdDashboard /> },
+      { title: "Users", path: "/dashboard/users", icon: <MdSupervisedUserCircle /> },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
     ],
   },
   {
     title: "Analytics",
     list: [
       {
-        title:"Revenue",
+        title: "Revenue",
         path: "/dashboard/revenue",
-        icon: <MdAttachMoney/>,
+        icon: <MdAttachMoney />,
       },
       {
-        title:"Revenue",
-        path: "/dashboard/revenue",
-        icon: <MdAttachMoney/>,
+        title: "Reports",
+        path: "/dashboard/reports",
+        icon: <MdAttachMoney />,
       },
       {
-        title:"Revenue",
-        path: "/dashboard/revenue",
-        icon: <MdAttachMoney/>,
+        title: "Teams",
+        path: "/dashboard/teams",
+        icon: <MdAttachMoney />,
       },
     ],
   },
   {
-    title: 'User',
+    title: "User",
     list: [
       {
-        title:"Settings",
+        title: "Settings",
         path: "/dashboard/settings",
-        icon: <MdOutlineSettings/>,
+        icon: <MdOutlineSettings />,
       },
       {
-        title:"Revenue",
-        path: "/dashboard/revenue",
-        icon: <MdOutlineSettings/>,
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdOutlineSettings />,
       },
       {
-        title:"Revenue",
-        path: "/dashboard/revenue",
-        icon: <MdOutlineSettings/>,
+        title: "Logout",
+        path: "/dashboard/logout",
+        icon: <MdLogout />,
       },
     ],
-  }
+  },
 ];
 
 const Sidebar = () => {
   return (
-  <div className="sidebar">
-  <ul>
-  {menuItems.map((cat)=>(
-      <li key={cat.title}>{cat.title}</li>
-))}
-  </ul>
-    
-  </div>
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image priority={false} className={styles.userImage} src="/avator.png" alt="userImage" width="50" height="50" />
+        <div className={styles.userDetails}>
+          <span className={styles.userName}>John Doe</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink key={item.title} item={item} />
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
