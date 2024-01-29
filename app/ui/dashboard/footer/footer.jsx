13707941c253367d6078eaccd5ee67a44e7e4b18
Rajesh Tiwari <tiwarirj73@gmail.com>
2024-01-29 22:13:55 +0530

    rightsidebar & footer completed

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './footer.module.css'
+function Footer() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Admin</div>
+      <div className={styles.text}>All Right Reserved</div>
+    </div>
+  )
+}
+
+export default Footer
--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import styles from './rightbar.module.css';
+import Image from 'next/image';
+import { MdPlayCircleFilled } from 'react-icons/md';
 
 function Rightbar() {
   return (
-    <div>
-      Rightbar
+    <div className={styles.container}>
+    <div className={styles.item}>
+      <div className={styles.bgContainer}>
+         <Image src="/avator.png" alt='' fill className={styles.bg}/>
+      </div>
+      <div className={styles.texts}>
+           <span className={styles.notification}>Available Now</span>
+           <h3 className={styles.title}>
+            how to use the new version of the admin dashboard?
+           </h3>
+           <span className={styles.subtitle}> 
+           Takes 4 minutes to learn
+           </span>
+           <p className={styles.desc}>
+            Lorem ipsum dolar sit amet consectetur adipisicing. Reprehenderit eius libero perspiciatis recusandae possimus.
+           </p>
+           <button className={styles.button}>
+           Watch
+            <MdPlayCircleFilled/>
+           </button>
+      </div>
+    </div>
+    <div className={styles.item}>
+      <div className={styles.bgContainer}>
+         <Image src="/avator.png" alt='' fill className={styles.bg}/>
+      </div>
+      <div className={styles.texts}>
+           <span className={styles.notification}>Available Now</span>
+           <h3 className={styles.title}>
+            how to use the new version of the admin dashboard?
+           </h3>
+           <span className={styles.subtitle}> 
+           Takes 4 minutes to learn
+           </span>
+           <p className={styles.desc}>
+            Lorem ipsum dolar sit amet consectetur adipisicing. Reprehenderit eius libero perspiciatis recusandae possimus.
+           </p>
+           <button className={styles.button}>
+           Learn
+            <MdPlayCircleFilled/>
+           </button>
+      </div>
+    </div>
+      
     </div>
   )
 }
